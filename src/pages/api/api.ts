6686af78ac7ee60bf4de0154fae5122900542785
Francis Yuppie <dcs00922022@students.tum.ac.ke>
2024-02-02 16:37:58 +0300

    new update

---
+++ src/pages/api/api.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function fetchData2(req: NextApiRequest, res: NextApiResponse) {
+  const url = 'https://catfact.ninja/fact';
+
+  try {
+    const response = await axios.get(url);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
